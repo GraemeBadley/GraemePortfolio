bbd4121da79d2ea545e04d6c1ad56402fdf8eddf
GraemeBadley <17gb12@queensu.ca>
2024-06-14 15:10:31 -0400

    began intro section

---
+++ src/components/Intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Intro(){
+    return (
+        <div className="flex items-center
+        justify-center text-center flex-col pt-20 pb-6">
+            <h1 className="font-bold text-4xl mb-1">Graeme Badley</h1>
+            <p className="text-base mb-3 font-medium">Software Engineer and Web Developer</p>
+            <p className="text-sm mb-6 font-bold max-w-xl">Bio</p>
+        </div>
+    )
+}
+
+export default Intro;
